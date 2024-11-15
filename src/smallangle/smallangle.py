ac7e529d0284bc0699b572168718273d0108b769
JoySingh <joyprietsingh@gmail.com>
2024-11-15 14:35:07 +0100

    created commando with option as steps

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,20 +1,51 @@
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+from numpy import pi
 
 
-def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+@click.group()
+def cmd_group():
+    pass
+
+
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--steps",
+    default=10,
+    help="Number of steps between 0 and 2pi",
+    show_default=True,  # show default in help
+)
+def sin(steps):
+    """Gives a 2 lists of x and sinx in given steps between 0 and 2pi
+
+    Args:
+        steps (int): The number of steps between 0 and 2pi
+    """
+    x = np.linspace(0, 2 * pi, steps)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 
-def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--steps",
+    default=10,
+    help="Number of steps between 0 and 2pi",
+    show_default=True,  # show default in help
+)
+def tan(steps):
+    """Gives a 2 lists of x and tanx in given steps between 0 and 2pi
+
+    Args:
+        steps (int): The number of steps between 0 and 2pi
+    """
+    x = np.linspace(0, 2 * pi, steps)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
